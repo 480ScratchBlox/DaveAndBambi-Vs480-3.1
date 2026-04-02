e9db247bc900e375f348100bb97e485ce7728ba8
480scratchblox <119143348+480ScratchBlox@users.noreply.github.com>
2026-04-02 18:44:31 -0400

    idk what to put here

---
+++ example_mods/scripts/StageLighting.lua
@@ -0,0 +1,13 @@
+if curStage == 'house-sunset' then
+doTweenColor('boyfriendColorTween', 'boyfriend', '0xff878787', 0.01, 'quadInOut')
+doTweenColor('dadColorTween', 'dad', '0xff878787', 0.01, 'quadInOut')
+doTweenColor('gfColorTween', 'gf','0xff878787', 0.01, 'quadInOut')
+elseif curStage == 'house-night' then
+doTweenColor('boyfriendColorTween', 'boyfriend', '0xff585e8c', 0.01, 'quadInOut')
+doTweenColor('dadColorTween', 'dad', '0xff585e8c', 0.01, 'quadInOut')
+doTweenColor('gfColorTween', 'gf','0xff585e8c', 0.01, 'quadInOut')
+elseif curStage == 'house-nostaliga' then
+doTweenColor('boyfriendColorTween', 'boyfriend', '0xff949df2', 0.01, 'quadInOut')
+doTweenColor('dadColorTween', 'dad', '0xff949df2', 0.01, 'quadInOut')
+doTweenColor('gfColorTween', 'gf','0xff949df2', 0.01, 'quadInOut')
+end
